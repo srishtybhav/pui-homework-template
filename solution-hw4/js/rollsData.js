9a944e4f37f5b3722d49962662a696874c640e8b
srishtybhav <129609362+srishtybhav@users.noreply.github.com>
2023-10-01 03:27:06 -0400

    comments

--- solution-hw4/js/rollsData.js
+++ solution-hw4/js/rollsData.js
@@ -25,29 +25,33 @@ const rolls = {
     }    
 };
 
-
+// query search html urls for the keys that correspond to the rolls list
 const queryString = window.location.search;
 const params = new URLSearchParams(queryString);
 const rollType = params.get('roll');
 
+// the specific heading title and change it to the corresponding text when clicking an item in gallery
 const headerElement = document.querySelector('#Productdetailtitle');
 headerElement.innerHTML = rollType + " Cinnamon Roll";
 
+// the specific image and change it to the corresponding image when clicking an item in gallery
 const rollImage = document.querySelector('#cinnamonrolldetailimage');
 rollImage.src = '../assets/products/' + rolls[rollType]["imageFile"];
 
+// the specific price on detail page and change the total calculated price based on new base prices in rolls list
 const rollPrice = document.querySelector('.detailedprice');
 rollPrice.innerHTML = '$' + rolls[rollType]["basePrice"];
 const rollbasePrice = rolls[rollType]["basePrice"];
 //console.log(rollPrice);
 
 
-
+// empty cart which will be loaded into when add to cart button
 cart = {
 
 
 }
 
+// class roll which constructs a new cart taking the parameters of the roll flavor, glazing, packing size, and base price (NOT the total price)
 class Roll {
     constructor(rollType, rollGlazing, packSize, basePrice) {
         this.type = rollType;
@@ -57,9 +61,10 @@ class Roll {
     }
 }
 
+//function that inputs the roll parameters whenever add to card is onclick
 function addtocart(This) {
 
     const rollCart = new Roll(rollType, rollGlazing, packSize, rollbasePrice );
-    console.log(rollCart);
+    console.log(rollCart); // check console to see that objects of the list match the items on screen
 ;
 }
